738fc3c2d3b92280cf2f7813d5801f8a3da7d16b
EsamElhadi <esamaldinelhady19@gmail.com>
2023-02-13 09:01:30 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let navbar = document.querySelector('.navbar');
+let toggle = document.querySelector('.togle');
+
+toggle.addEventListener('click', () => {
+    navbar.classList.toggle('hidden')
+})
+
+// const esam = ["Esam", "Ahmed", "Mohamed"];
+// for (let i = 0; i < esam.length; i++) {
+//     const element = esam[i];
+//     console.log(element);
+// }
